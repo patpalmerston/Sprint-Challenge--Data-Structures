d71baf1035b6596322c3df25002c32644057e8f2
Pat Palmerston <patpalmerston@gmail.com>
2019-12-20 08:35:39 -0800

    names runtime down under a second

--- names/names.py
+++ names/names.py
@@ -1,5 +1,6 @@
 import time
-from binary_search_tree import BST
+#this seems like a great fit for a BST
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -13,11 +14,23 @@ f.close()
 
 duplicates = []
 # original
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
+# pat version 1
+# grab the beginning index of names_2 to iterate through for duplicates against names_1
+bst = BinarySearchTree(names_2[0])
+# for every name in names_1 insert name into tree
+for name in names_1:
+    bst.insert(name)
+# for every name in names_2
+for name in names_2:
+    # if bst already contains the name
+    if bst.contains(name):
+        # append the duplicates to the duplicate array
+        duplicates.append(name)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
