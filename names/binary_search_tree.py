52bab7a6f22fd1a3cd3e6df0bcad8a147936ed80
Pat Palmerston <patpalmerston@gmail.com>
2019-12-20 08:24:36 -0800

    set up bst for names

---
+++ names/binary_search_tree.py
@@ -0,0 +1,51 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if self.value:
+            if value < self.value:
+                if self.left is None:
+                    self.left = BinarySearchTree(value)
+                else:
+                    self.left.insert(value)
+            elif value > self.value:
+                if self.right is None:
+                    self.right = BinarySearchTree(value)
+                else:
+                    self.right.insert(value)
+        else:
+            self.value = value
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif target < self.value and self.left:
+            return self.left.contains(target)
+        elif target > self.value and self.right:
+            return self.right.contains(target)
+        return False
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if (self.right):
+            return self.right.get_max()
+        return self.value
+                
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
+        if not self.right and not self.left:
+            return
+
